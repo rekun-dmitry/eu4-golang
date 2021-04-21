dac73bf6d95e23b8226bb04e802b3c699de6f855
rekun-dmitry <dsrekun@gmail.com>
2021-04-21 17:39:26 +0300

    fixed bugs in parser

--- parsers/ironman_parser.go
+++ parsers/ironman_parser.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/csv"
 	"encoding/hex"
 	"fmt"
@@ -17,6 +18,7 @@ func check(e error) {
 }
 
 func main() {
+
 	file, err := os.Open("../sources/gamestate")
 	conv_file, err_conv := os.Open("../sources/eu4bin.csv")
 	if err_conv != nil {
@@ -43,7 +45,13 @@ func main() {
 		}
 		if index_string > 0 {
 			splitted_string := strings.Split(row[0], ";")
-			decoders[strings.ToLower(splitted_string[0][2:])] = splitted_string[1]
+			if len(splitted_string[1]) > 0 && splitted_string[1] != "String" && splitted_string[1] != "Integer" && splitted_string[1] != "Float" && splitted_string[1] != "Boolean" {
+				decoders[strings.ToLower(splitted_string[0][2:])] = splitted_string[1]
+			} else if splitted_string[1] == "String" || splitted_string[1] == "Integer" || splitted_string[1] == "Float" || splitted_string[1] == "Boolean" {
+				decoders[strings.ToLower(splitted_string[0][2:])] = " "
+			} else if splitted_string[0] == "0x0600" {
+				decoders[strings.ToLower(splitted_string[0][2:])] = string("\n")
+			}
 		}
 
 		index_string++
@@ -58,28 +66,55 @@ func main() {
 	//start decode
 	var size int64 = stats.Size()
 
-	bytes := make([]byte, size)
+	file_bytes := make([]byte, size)
 
 	bufr := bufio.NewReader(file)
-	_, err = bufr.Read(bytes)
+	_, err = bufr.Read(file_bytes)
 
-	dst := make([]byte, hex.EncodedLen(len(bytes)))
-	hex.Encode(dst, bytes)
+	dst := make([]byte, hex.EncodedLen(len(file_bytes)))
+	hex.Encode(dst, file_bytes)
+	byteReader := bytes.NewReader(dst)
+	hex_reader := make([]byte, 2)
+	dst_decoded := make([]byte, hex.EncodedLen(len(file_bytes)))
+	hex_buf := bytes.Buffer{}
 
-	var dst_dec []byte
-	fmt.Println("starting decode")
-	for key, val := range decoders {
-		dst_dec = []byte(strings.ReplaceAll(string(dst), key, hex.EncodeToString([]byte(val))))
+	fmt.Println("starting decode", len(dst))
+	tmp := ""
+	for {
+		_, err := byteReader.Read(hex_reader)
+		if err == io.EOF {
+			dst_decoded = hex_buf.Bytes()
+			break
+		}
+
+		tmp += string(hex_reader)
+		if len(tmp) <= 2 {
+			_, err = byteReader.Read(hex_reader)
+			if err == io.EOF {
+				dst_decoded = hex_buf.Bytes()
+				break
+			}
+			tmp += string(hex_reader)
+		}
+
+		if val, ok := decoders[tmp]; ok {
+			hex_buf.Write([]byte(hex.EncodeToString([]byte(val))))
+			tmp = ""
+		} else {
+			hex_buf.Write([]byte(tmp[:2]))
+			tmp = tmp[2:]
+		}
 	}
-	fmt.Println("finished decode")
 
-	decoded := make([]byte, hex.DecodedLen(len(dst_dec)))
-	n, err_dec := hex.Decode(decoded, dst_dec)
+	fmt.Println("finished decode", len(dst))
+
+	decoded := make([]byte, hex.DecodedLen(len(dst_decoded)))
+	n, err_dec := hex.Decode(decoded, dst_decoded)
 	if err_dec != nil {
 		fmt.Println("error in decoding")
 	}
 	fmt.Println(n)
-	f, err := os.Create("/output.txt")
+	f, err := os.Create("./output.txt")
 	check(err)
 	defer f.Close()
 
